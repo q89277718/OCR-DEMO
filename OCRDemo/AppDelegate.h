3d56319d66f112de23833fdc9dcbdc833579156b
夏路遥 <357804737@163.com>
2017-05-02 14:31:14 +0800

    初始化工程

---
+++ OCRDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCRDemo
+//
+//  Created by 夏路遥 on 2017/5/2.
+//  Copyright © 2017年 Xialuyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCRDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCRDemo
+//
+//  Created by 夏路遥 on 2017/5/2.
+//  Copyright © 2017年 Xialuyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
